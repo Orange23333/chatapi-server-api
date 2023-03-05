acd5ba9099ebd2ee12738cf746bcd99331c9ae51
Orange_233 <874462262@qq.com>
2023-03-05 12:44:21 +0800

    upload.

--- src/csharp/ChatApi.Server.Api/Data/UserProfile.cs
+++ src/csharp/ChatApi.Server.Api/Data/UserProfile.cs
@@ -1,6 +1,16 @@
-﻿namespace ChatApi.Server.Api.Data
+﻿using EleCho.Json;
+using YamlDotNet;
+
+namespace ChatApi.Server.Api.Data
 {
 	public class UserProfile
 	{
+		public Guid UserId { get; set; }
+
+		public string UserName { get; set; }
+
+		public (string HashType, string PasswordHash) PassWord { get; set; }
+
+		public 
 	}
 }
